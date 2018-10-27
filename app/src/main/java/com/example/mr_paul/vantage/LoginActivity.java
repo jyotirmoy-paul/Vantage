c838113b9aad0f05133ef92254af075d5cb3df8f
Jyotirmoy Paul <41543437+jyotirmoy-paul@users.noreply.github.com>
2018-10-27 16:29:53 +0530

    Fixed Bugs
    1. Disabled the utilisation of full screen
    2. Fixed the EditText adjustPan Bug
    3. Duplicate Email Registration changes

--- app/src/main/java/com/example/mr_paul/vantage/LoginActivity.java
+++ app/src/main/java/com/example/mr_paul/vantage/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.mr_paul.vantage;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -17,14 +20,18 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ErrorDialogFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Random;
 
@@ -35,17 +42,12 @@ public class LoginActivity extends AppCompatActivity {
     // declaration of important variables
     FirebaseAuth mAuth;
     SharedPreferences sharedPreferences;
+    String emailId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        // to make the screen go full
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
 
         sharedPreferences = getSharedPreferences("userInformation",MODE_PRIVATE);
 
@@ -86,13 +88,13 @@ public class LoginActivity extends AppCompatActivity {
 
                 // check if this user is present, and if present delete the user (coz first
                 // time it might be a mistake due to poor internet connectivity )
-                deleteUser();
+
 
                 String domain = "@iiitkalyani.ac.in";
 
 
                 // check the email provided, to match "@iiitkalyani.ac.in" domain
-                final String emailId = userEmail.getText().toString().trim();
+                emailId = userEmail.getText().toString().trim();
 
                 StringBuilder stringBuilder = new StringBuilder();
                 stringBuilder.append(emailId);
@@ -127,14 +129,17 @@ public class LoginActivity extends AppCompatActivity {
                 // email and name of the user is checked for no error
                 // now verify the credentials
 
-                final ProgressBar progressBar = findViewById(R.id.progressBar);
-                progressBar.setVisibility(View.VISIBLE);
+                // show an progress dialog
+                final ProgressDialog pd = new ProgressDialog(LoginActivity.this);
+                pd.setMessage("Creating Account");
+                pd.show();
 
                 Random random = new Random(); // a user is created with random password (as password
                 // is not necessary: one-time registration )
                 final String password = random.nextLong() + "";
                 // storing the password inside the app, (will be used for re-authentication or
                 // if user wants to delete account)
+
 
                 mAuth.createUserWithEmailAndPassword(emailId,password)
                         .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
@@ -159,7 +164,7 @@ public class LoginActivity extends AppCompatActivity {
                                                 public void onComplete(@NonNull Task<Void> task) {
 
                                                     // make the progressBar invisible
-                                                    progressBar.setVisibility(View.INVISIBLE);
+                                                    pd.cancel();
 
                                                     // store the user name
                                                     SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -184,11 +189,21 @@ public class LoginActivity extends AppCompatActivity {
 
                                 }
                                 else{
-                                    final ProgressBar progressBar = findViewById(R.id.progressBar);
-                                    progressBar.setVisibility(View.INVISIBLE);
 
-                                    Toast.makeText(LoginActivity.this,
-                                            "Authentication failed, try again later", Toast.LENGTH_SHORT).show();
+                                    pd.cancel();
+
+                                    // show collision error text if email id entered by user is already registered!
+                                    try{
+                                        throw task.getException();
+                                    }
+                                    catch(FirebaseAuthUserCollisionException e){
+                                        showEmailCollisionError();
+                                    }
+                                    catch(Exception e){
+                                        Toast.makeText(LoginActivity.this,
+                                                "Something unexpected happened! Try again later.", Toast.LENGTH_SHORT).show();
+                                    }
+
                                 }
                             }
                         });
@@ -196,32 +211,38 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public void deleteUser(){
+    public void showEmailCollisionError(){
 
-        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        String errorMessage = "If '" + emailId + "' is your email id, then you either have cleared the app data, or forced the app to close after entering your Registration Details. \nWhat so ever be the case, contact the developer for help.";
 
-        if(firebaseUser == null){
-            return;
+        // show an error dialog box
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(LoginActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(LoginActivity.this);
         }
+        builder.setTitle("Registration Error!")
+                .setMessage(errorMessage)
+                .setPositiveButton("Contact the developer", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
 
-        final ProgressDialog pd = new ProgressDialog(LoginActivity.this);
-        pd.setMessage("Clearing Something Important");
+                        // call an intent to g-mail for writing to developer
+                        // call an intent to Gmail app
+                        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + "jyotirmoy@iiitkalyani.ac.in"));
+                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Vantage Registration Error");
+                        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email: " + emailId + "\n\n");
+                        startActivity(Intent.createChooser(emailIntent, "Chooser Title"));
 
-        // re authenticate the user, as may require sometimes
-        AuthCredential credential = EmailAuthProvider
-                .getCredential(sharedPreferences.getString("userEmail","default email"),
-                        sharedPreferences.getString("password","default password"));
-        firebaseUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                firebaseUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        pd.cancel();
                     }
-                });
-            }
-        });
+                })
+                .setNegativeButton("Ooops! It wasn't my Email Id", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 
 }
